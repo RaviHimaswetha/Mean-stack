fcb08f43481fc8014538d2c30621ed013add88cc
RaviHimaswetha <68724538+RaviHimaswetha@users.noreply.github.com>
2021-06-01 15:17:36 +0530

    Add files via upload

--- part1/part1.3/src/App.js
+++ part1/part1.3/src/App.js
@@ -1,1 +1,27 @@
+const course = 'full Stack application development'
+  const task1 = 
+  {
+    name:'Fundamentals of React',
+    exercise: 15
+  }
+  const task2 = 
+  {
+    name: 'Using props to pass data',
+    exercise: 20
+  }
+  const task3 = 
+  {
+    name: 'State of a component',
+    exercise: 25
+  }
+  const total = 
+  {
+    name: 'Total number of exercises'
+  
+  }
 
+       console.log(course)
+       console.log(task1.name , task1.exercise)
+       console.log(task2.name , task2.exercise)
+       console.log(task3.name , task3.exercise)
+       console.log(total.name , task1.exercise + task2.exercise + task3.exercise)
