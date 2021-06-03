9a102700150411ee82499bff3dd5b4ae290a318d
RaviHimaswetha <68724538+RaviHimaswetha@users.noreply.github.com>
2021-06-03 17:26:06 +0530

    Add files via upload

--- part2/part2.12/src/App.js
+++ part2/part2.12/src/App.js
@@ -1,1 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Find from "./components/Find";
 
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [newfilter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+      setCountries(response.data);
+    });
+  }, []);
+
+
+
+  const handleChange = event => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Find countries</h2>
+      <input value={newfilter} onChange={handleChange}/>
+          <Find countries={countries} newfilter={newfilter} />
+    </div>
+  )
+}
+
+export default App;
