755b9b1732a8ccaa6f2ba0ddc9918cc438a8f604
RaviHimaswetha <68724538+RaviHimaswetha@users.noreply.github.com>
2021-06-03 16:03:58 +0530

    Add files via upload

--- part2/part2.7/src/App.js
+++ part2/part2.7/src/App.js
@@ -1,1 +1,69 @@
+import React, {useState} from 'react'
 
+const Person = ({ Mearn }) => {
+  return (
+    <div>
+      &nbsp;&nbsp;{Mearn.content}
+
+    </div>
+  )
+}
+
+const App = (dev) => {
+  const [ persons, setPersons ] = useState(dev.persons)
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    if ( persons.find(sort=>sort.content===newName ) ) 
+    {
+      window.alert(newName + ' is already added to phonebook');
+    }
+  else
+  {
+    const nameObject = 
+    {
+      content: newName,
+      id: persons.length + 1,
+    }
+  
+  setPersons(persons.concat(nameObject))
+  }
+  
+    setNewName('')    
+  
+  
+  }
+  const namesToShow = persons
+
+
+ 
+  
+  
+  return (
+    <div>
+      <h2>&nbsp;Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          &nbsp;name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          &nbsp;<button type="submit">add</button>
+        </div>
+      </form>
+      <h2>&nbsp;&nbsp;Numbers</h2>
+      {namesToShow.map(Mearn =>
+          <Person key={Mearn.id} Mearn={Mearn} />
+        )}
+      
+
+    </div>
+  )
+}
+
+export default App
