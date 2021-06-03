85b2b711656ced99a6a1fe1c87e583c5b41c93cc
RaviHimaswetha <68724538+RaviHimaswetha@users.noreply.github.com>
2021-06-03 15:21:08 +0530

    Add files via upload

--- part2/part2.1/src/App.js
+++ part2/part2.1/src/App.js
@@ -1,1 +1,70 @@
+import React from 'react'
 
+const Header = ({ web }) => {
+  return (
+      <h2>{web.name}</h2>
+  )
+}
+
+const Part = (props) => {
+  return (
+      <p>
+          {props.part.name} {props.part.exercise}
+      </p>
+  )
+}
+
+const Content = ({ web }) => {
+  return (
+      <div>
+          {web.parts.map((part) => <Part part={part} />)}
+      </div>
+  )
+}
+
+
+const Course = ({ web }) => {
+  return (
+      <>
+          <Header web={web} />
+          <Content web={web} />
+         
+      </>
+  )
+}
+const App = () => {
+  const task = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercise: 20,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercise: 15,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercise: 25,
+          id: 3
+        },
+        
+      ]
+    },
+    
+  ]
+
+  return (
+    <>
+      
+      {task.map(web => <Course web={web} />)}
+    </>
+  )
+}
+
+export default App
