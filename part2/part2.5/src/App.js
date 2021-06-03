7efaf3429bd7a1da0b5019b35cdfee5f4e2213d5
RaviHimaswetha <68724538+RaviHimaswetha@users.noreply.github.com>
2021-06-03 15:56:21 +0530

    Add files via upload

--- part2/part2.5/src/App.js
+++ part2/part2.5/src/App.js
@@ -1,1 +1,58 @@
+import React from 'react'
+import Mearn from './components/Mearn.js'
 
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercise: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercise: 15,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercise: 20,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercise: 10,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercise: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercise: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map(web => <Mearn web={web} />)}
+    </>
+  )
+}
+
+export default App
