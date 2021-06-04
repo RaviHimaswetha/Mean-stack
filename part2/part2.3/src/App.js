c6c994b61af14deb32911cf40aa3f782a6e64ae5
RaviHimaswetha <68724538+RaviHimaswetha@users.noreply.github.com>
2021-06-04 16:56:36 +0530

    Add files via upload

--- part2/part2.3/src/App.js
+++ part2/part2.3/src/App.js
@@ -1,1 +1,80 @@
+import React from 'react'
 
+const Header = ({ Mearn }) => {
+  return (
+      <h2>{Mearn.name}</h2>
+  )
+}
+
+const Part = (pro) => {
+  return (
+      <p>
+          {pro.part.name} {pro.part.exercise}
+      </p>
+  )
+}
+
+const Content = ({ Mearn }) => {
+  return (
+      <div>
+          {Mearn.parts.map((part) => <Part part={part} />)}
+      </div>
+  )
+}
+
+const Total = ({ Mearn }) => {
+  const sum1 = Mearn.parts.reduce((total, part) => total + part.exercise, 0)
+  return (
+      <strong>Number of exercises {sum1}</strong>
+  )
+}
+
+const Course = ({ Mearn }) => {
+  return (
+      <>
+          <Header Mearn={Mearn} />
+          <Content Mearn={Mearn} />
+          <Total Mearn={Mearn} />
+      </>
+  )
+}
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercise: 20,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercise: 25,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercise: 10,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercise: 15,
+          id: 4
+        }
+      ]
+    },
+    
+  ]
+
+  return (
+    <>
+      
+      {courses.map(Mearn => <Course Mearn={Mearn} />)}
+    </>
+  )
+}
+
+export default App
